7c898f989c768d36833024a9beaf12b8abdb4d05
renan faria <renan.faria@planium.io>
2022-09-22 14:02:38 -0300

    Update: Implememntação de funcionabilidade de edição de dados do produto.

--- app/Http/Controllers/CadastroController.php
+++ app/Http/Controllers/CadastroController.php
@@ -108,6 +108,61 @@ class CadastroController extends Controller
            
         
     }
+
+    //Função que edita o produto
+    public function update(Request $request, $id)
+    {
+
+
+        $user = auth()->user();
+
+        // instância do objeto Product da base de dados
+        $produto = new Product;
+
+        // busca por produto vinculado ao vendedor e o pelo id de parmetro na view   
+        $produto = Product::findOrFail($id)->where([
+
+            ['id', $id],
+            ['user_id', $user->id]
+
+        ]
+            
+        )->first();
+
+        $produto->nomeproduto = $request->nomeproduto;
+        $produto->precoProduto = $request->precoProduto;
+        $produto->descProduto = $request->descProduto;
+
+        // Image Upload
+
+        // Primeiro cria-se uma condicional para caso o arquivo exista
+        if($request->hasfile('fotoproduto') && $request->file('fotoproduto')->isValid()){
+
+            // pega a extensão da imagem
+            $requestImage = $request->fotoproduto;
+
+            $extension = $requestImage->extension();
+
+            // gera uma string única baseada no tempo de upload através da hash conctenada com extensão do arquivo
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now") . $extension);
+
+
+            // salvando imagem no servidor
+            $requestImage->move(public_path('img/fotoproduto'), $imageName);
+
+            $produto->fotoproduto = $imageName;
+        
+
+        }
+
+        // por final os dados do objeto intanciado é salvo
+        $produto->save();
+
+         // redireciona após o cadastro do produto
+         return redirect('/listaProdutos')->with('msg', 'Produto editado com sucesso!');
+
+    }
+
     
     // traz os dados do formulário
     public function store(Request $request){
